494ab817dd7809b4358e31fc992acd710749a66a
Chaitanya Singh Negi <143953392+CSNegi27@users.noreply.github.com>
2024-10-01 22:34:15 +0530

    AboutUs.js

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,30 +1,7 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './AboutUs.css';
 
 const AboutUs = () => {
-  useEffect(() => {
-    const div = document.createElement('div');
-    div.id = 'aichatbot';
-    document.body.appendChild(div);
-
-    window.chatbotConfig = ['57DD6C33-E58D-4AC8-B663-86754C01FB55', 'onboarding_bot', {
-      apiHost: 'https://api-cf-ap-8.sendbird.com',
-    }];
-
-    const script = document.createElement('script');
-    script.defer = true;
-    script.type = 'module';
-    script.src = 'https://aichatbot.sendbird.com/index.js';
-
-    const firstScript = document.getElementsByTagName('script')[0];
-    firstScript.parentNode.insertBefore(script, firstScript);
-
-    return () => {
-      document.body.removeChild(div);
-      firstScript.parentNode.removeChild(script);
-    };
-  }, []);
-
   return (
     <section className="about">
       <h2>About Us</h2>
@@ -67,4 +44,4 @@ const AboutUs = () => {
   );
 };
 
-export default AboutUs;
+export default AboutUs; 
