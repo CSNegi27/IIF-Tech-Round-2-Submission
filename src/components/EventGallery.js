a36c6532f0904c9a26d652aaee506beb1982d31f
Chaitanya Singh Negi <143953392+CSNegi27@users.noreply.github.com>
2024-10-01 22:36:55 +0530

    EventGallery.js

--- src/components/EventGallery.js
+++ src/components/EventGallery.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './EventGallery.css'; 
 import image1 from './assets/event1.jpg';
 import image2 from './assets/event2.png';
@@ -7,29 +7,6 @@ import image4 from './assets/image4.jpg';
 import image5 from './assets/image5.jpg';
 
 const EventGallery = () => {
-  useEffect(() => {
-    const div = document.createElement('div');
-    div.id = 'aichatbot';
-    document.body.appendChild(div);
-
-    window.chatbotConfig = ['57DD6C33-E58D-4AC8-B663-86754C01FB55', 'onboarding_bot', {
-      apiHost: 'https://api-cf-ap-8.sendbird.com',
-    }];
-
-    const script = document.createElement('script');
-    script.defer = true;
-    script.type = 'module';
-    script.src = 'https://aichatbot.sendbird.com/index.js';
-
-    const firstScript = document.getElementsByTagName('script')[0];
-    firstScript.parentNode.insertBefore(script, firstScript);
-
-    return () => {
-      document.body.removeChild(div);
-      firstScript.parentNode.removeChild(script);
-    };
-  }, []);
-
   const images = [image1, image2, image3, image4, image5];
 
   return (
@@ -38,7 +15,7 @@ const EventGallery = () => {
       <div className="gallery-grid">
         {images.map((src, index) => (
           <div key={index} className="gallery-item">
-            <img src={src} alt={`Event ${index + 1}`} />
+            <img src={src} alt={Event ${index + 1}} />
           </div>
         ))}
       </div>
